59da41f4a0b09680539c06d205d30f996f12ef1e
rvserna <viensmrobert@gmail.com>
2024-05-08 12:53:41 -0400

    serialized dictionary using pickle

--- dict.py
+++ dict.py
@@ -1,6 +1,16 @@
-thisdict = {
+import pickle
+
+dict = {
   "brand": "Ford",
   "model": "Mustang",
   "year": 1964,
   "colour": "Purple"
 }
+
+with open("dict_serialized.pkl", "wb") as handle:
+  pickle.dump(dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+with open("dict_serialized.pkl", "rb") as handle:
+  dict_unserialized = pickle.load(handle)
+
+print(dict == dict_unserialized)
