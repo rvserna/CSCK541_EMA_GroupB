1d456fb35f5b8e6fea3b5f960794b66d2da52cd8
Nadim Rai <nadim_rai@yahoo.com>
2024-05-08 23:52:46 +0800

    imported json module in client file and created helper function that sends message from client to server

--- client.py
+++ client.py
@@ -1,4 +1,8 @@
 import socket
+import json
+
+def send_message(client_socket, message):
+    client_socket.send(json.dumps(message).encode('utf-8'))
 
 def run_client():
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -7,6 +11,13 @@ def run_client():
     client_socket.connect((host, port))
 
     print(f"Client connected to server socket")
+
+    while True:
+        message = input("Enter your message or type 'exit' to disconnect: ")
+
+        message = {'message': message}
+        send_message(client_socket, message)
+
 
 
 if __name__ == "__main__":
