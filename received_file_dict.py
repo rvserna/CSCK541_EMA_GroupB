036721bf434712124f7c76b90996a7f92e752b5c
sapphira <raizel.fs1@gmail.com>
2024-05-16 01:54:49 +0100

    Add docstring to unit test, change generate_key filename

--- received_file_dict.py
+++ received_file_dict.py
@@ -6,11 +6,8 @@ dict = {
   "year": 1964,
   "colour": "Purple"
 }
-
+# Open the pickle file in
+# binary writing mode
 with open("dict_serialized.pkl", "wb") as handle:
-  pickle.dump(dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
-with open("dict_serialized.pkl", "rb") as handle:
-  dict_unserialized = pickle.load(handle)
-
-print(dict == dict_unserialized)
+    # Serialize the dictionary
+    pickle.dump(dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
